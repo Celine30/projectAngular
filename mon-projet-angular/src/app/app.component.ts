8f9a9dedfd568d9374c9e7c1fb60f7dc1e043fe5
Celine <celi.caurier@gmail.com>
2020-03-03 21:17:29 +0100

    START

--- mon-projet-angular/src/app/app.component.ts
+++ mon-projet-angular/src/app/app.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'mon-projet-angular';
+  isAuth = false;
+
+    
+  appareilOne = 'Machine à laver';
+  appareilTwo = 'Frigo';
+  appareilThree = 'Ordinateur';
+
+
+ 
+  
+
+  constructor() {
+    
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+  }
 }
---
+++ mon-projet-angular/src/app/appareil/appareil.component.ts
@@ -0,0 +1,26 @@
+import { Input, Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+
+  @Input() appareilName: string;
+  @Input() appareilStatus: string;
+
+  constructor() {
+    
+
+   }
+
+  ngOnInit(): void {
+  }
+
+  getStatus() {
+    return this.appareilStatus;
+  }
+  
+}
