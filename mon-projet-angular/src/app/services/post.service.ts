c72690c8ac4eb832916d06861dc3154ba90ee92a
Celine <celi.caurier@gmail.com>
2020-03-27 21:45:00 +0100

    Add Astier

--- mon-projet-angular/src/app/services/post.service.ts
+++ mon-projet-angular/src/app/services/post.service.ts
@@ -3,10 +3,21 @@ import { Post} from '../post'
 export class PostService {
     
     posts = [
-        new Post('Le 1er Post','Lorem',2,new Date()),
-        new Post('Le 2eme Post','Lorem',2,new Date()),
-        new Post('Le 3eme Post','Lorem',2,new Date()),
-        new Post('Le 4eme Post','Lorem',2,new Date()),
+        new Post(
+            1,
+            'assets/astierA.jpg',
+            'ALEXANDRE ASTIER',
+            `La passion première d\'Alexandre Astier n\'est pas la comédie mais la musique.Très jeune, il entre au Conservatoire avant de poursuivre ses études à l\'American School of Modern Music à Paris. En parallèle, il suit ses premiers cours de théâtre et se met peu à peu à l\'écriture. Très vite, il monte deux pièces écrites par ses soins : en 1997, Poule Fiction, et en 2000, L\'étrange assistant du Docteur Lannion. C\'est grâce à sa prestation sur scène à Lyon dans Le jour du froment qu\'il se fait remarquer par les professionnels. Il se distingue notamment dans la pièce de Jacques Chambon, Nous crions grâce. Après avoir composé des musiques pour des courts-métrages tels que Soyons sport et Un soupçon fondé sur quelque chose de gras, il se lance dans la réalisation de son propre court, Dies iræ, qui remporte le Prix du public 2003 du Festival Off-Courts. Celui-ci donnera naissance à la série Kaamelott qui l\'a fait connaître auprès du grand public. Il est à la fois réalisateur, scénariste et acteur de cette fiction qui met en scène la légende des chevaliers de la Table Ronde. Elle a remplacé avec succès Caméra Café sur l\'antenne de M6 à partir du mois de Janvier 2005. Tandis qu\'il s\'offre le rôle du Roi Arthur, il en profite pour faire jouer sa famille. Ainsi, son père Lionel Astier obtient le rôle de Léodagan, son frère Simon Astier celui d\'Yvain, Joëlle Sevilla, sa mère, celui de Sévi et Josée Drevon, sa belle-mère, celui d\'Ygerne. Il va même jusqu\'à composer les musiques de la série ! A partir de 2006, il décline l\'univers de Kaamelott dans une BD dont il a écrit le scénario. Il décroche son premier rôle au cinéma en 2006 dans le film Comme t\'y es belle !, aux cotés de Michèle Laroque, puis il enchaîne avec le blockbuster français Astérix aux Jeux Olympiques. En 2008, il endosse le rôle de Reiser, grand dessinateur et ami de Coluche, dans le film d\'Antoine de Caunes, Coluche, l\'histoire d\'un mec. L\'année suivante, il offre un registre inattendu avec son rôle d\'ex-mari confident de Sophie Marceau dans LOL - Laughing Out Loud. Il fera un petit retour à la télévision en 2010 le temps d\'un caméo dans la série de son frère Simon, Hero Corp avant d\'incarner en 2011 le méchant Clotindre dans Philibert, pastiche de films de cape et d\'épée face à Jérémie Renier.  En 2012, Alexandre Astier voit enfin l’accomplissement de son premier long-métrage au cinéma : David et Madame Hansen. Comme à son habitude, Alexandre est sur tous les fronts puisqu\’il écrit, produit, réalise, monte et compose même les musiques de ce film. En 2012 aussi, il retrouve les planches avec sa pièce Que ma joie demeure !, et fait de nombreuses apparitions dans des émissions et mini-séries à succès comme Bref, Le Golden Show, ou encore Scènes de ménages en 2013. La même année, il collabore au scénario de Pop Rédemption et y joue avec Julien Doré. Le 6 mars 2014 est mis en ligne un court-métrage de Lisa Azuelos intitulé 14 millions de cris dans lequel il joue aux côtés de Julie Gayet et qui dénonce le mariage forcé de plus de 14 millions de mineurs chaque année dans le monde. Le mois suivant sort dans les salles la quatrième collaboration d\’Alexandre Astier et Lisa Azuelos dans la romance Une rencontre. A Noël 2014, le public français découvre Astérix - Le Domaine des Dieux, le neuvième film d’animation du célèbre petit gaulois. Alexandre Astier, à l’origine de ce projet, réunit une pléiade de stars dont des habitués de Kaamelott comme Lionel Astier et Joëlle Sevilla, mais aussi Elie Sémoun, Alain Chabat, Géraldine Nakache, ainsi que Florence Foresti et Roger Carel, la voix officielle d’Astérix depuis 1967. En 2014 toujours, l\’hyperactif Astier tourne dans le premier long-métrage de Kheiron et débute la tournée dans l\’hexagone, la Suisse et la Belgique de sa nouvelle pièce de théâtre l\’Exoconférence.`,
+            '16-06-1974',
+            '45 ans',
+            'Français',
+            'Acteur, Scénariste, Réalisateur',
+            2
+        )
+
+/*         new Post(2,'Le 2eme Post','Lorem',2,new Date()),
+        new Post(3,'Le 3eme Post','Lorem',2,new Date()),
+        new Post(4,'Le 4eme Post','Lorem',2,new Date()), */
         ] 
    
 }
